d069115520f76c2eda17d9a9d90de1a97c9582f2
Kyle Cierzan <kcierzan@gmail.com>
2015-12-30 11:51:51 -0800

    Add 4.5 pairing files

---
+++ week-4/good-guess/my_solution.rb
@@ -0,0 +1,13 @@
+# Good Guess
+
+# I worked on this challenge [with: Mike Pintar ].
+
+
+# Your Solution Below
+def good_guess?(guess)
+  if guess == 42
+    return true
+  else
+    return false
+  end
+end
---
+++ week-4/leap-years/my_solution.rb
@@ -0,0 +1,19 @@
+# Leap Years
+
+# I worked on this challenge [ with: Mike Pintar ].
+
+
+# Your Solution Below
+def leap_year?(year)
+  if year % 4 == 0
+    if year % 100 == 0
+      if year % 400 == 0
+        return true
+      else
+        return false
+      end
+    else return true
+    end
+  else return false
+  end
+end
